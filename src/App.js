0da1c16fed965a38fd63cf0639b223418275d80b
Pranam451 <ppranam451@gmail.com>
2022-07-02 19:32:27 +0530

    created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+
+const api={
+  key:'98921991d88bc3b801bf5395d85842e8',
+  base:'https://api.openweathermap.org/data/2.5/'
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
